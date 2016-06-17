5ef38d17671a2a16463a06713baa01054e980ad6
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-06-17 05:08:51 -0400

    Update ShowPretty.jl

--- src/ShowPretty.jl
+++ src/ShowPretty.jl
@@ -7,34 +7,49 @@ const separator = '_'
 function nonnegIntegerString(s::String, groupsize::Int=5)
    n = length(s)
    n==0 && return "0"
-   
+
+   sinteger, sexponent =
+        if contains(s,"e")
+           split(s,"e")
+        else
+           s, ""
+        end
+    
+   n = length(sinteger)
+
    fullgroups, finalgroup = divrem(n, groupsize)
 
-   sv = convert(Vector{Char},s)
+   sv = convert(Vector{Char},sinteger)
    p = repeat(" ", n+(fullgroups-1)+(finalgroup!=0))
    pretty = convert(Vector{Char},p)
    
    sourceidx = n
    targetidx = length(pretty)
    for k in fullgroups:-1:1
-       pretty[(targetidx-groupsize+1):targetidx] = sv[(sourceidx-groupsize+1):sourceidx]
-       sourceidx -= groupsize
-       targetidx -= groupsize
-       if k > 1
-           pretty[targetidx] = separator
-           targetidx -= 1
-       end
+        pretty[(targetidx-groupsize+1):targetidx] = sv[(sourceidx-groupsize+1):sourceidx]
+        sourceidx -= groupsize
+        targetidx -= groupsize
+        if k > 1
+            pretty[targetidx] = separator
+            targetidx -= 1
+        end
    end
    
    if finalgroup > 0
-       if fullgroups > 0 
-           pretty[targetidx] = separator
-           targetidx -= 1
-       end     
-       pretty[(targetidx-finalgroup+1):targetidx] = sv[(sourceidx-finalgroup+1):sourceidx]
+        if fullgroups > 0 
+            pretty[targetidx] = separator
+            targetidx -= 1
+        end     
+        pretty[(targetidx-finalgroup+1):targetidx] = sv[(sourceidx-finalgroup+1):sourceidx]
    end
 
-   convert(String, pretty)
+   prettystring = convert(String, pretty)
+
+    if sexponent != ""
+       join((prettystring,sexponent),"e")
+    else
+       prettystring
+    end
 end
 
 function integerString(s::String, groupsize::Int=5)
@@ -61,6 +76,21 @@ function fractionalString(s::String, groupsize::Int=5)
        join((pretty,sexponent),"e")
     else
        pretty
+    end
+end
+
+function realpretty(s::String, groupsize::Int=5)
+    sinteger, sfrac =
+        if contains(s,".")
+           split(s,".")
+        else
+           s, ""
+        end
+        
+    if sfrac == ""
+       integerString(sinteger, groupsize)
+    else
+       join( (integerString(sinteger, groupsize), fractionString(sfrac, groupsize)), "." )
     end
 end
 
