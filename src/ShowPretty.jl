98140062ef9d1aded896cbe6bf81c556b915af6e
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-06-17 03:57:17 -0400

    Create ShowPretty.jl

---
+++ src/ShowPretty.jl
@@ -0,0 +1,58 @@
+module ShowPretty
+
+export showpretty
+
+const separator = "_"
+
+function integerString(s::String, groupsize::Int=5)
+   numsign = (s[1] == "-") ? "-" : ""
+   if (s[1]=="-") s=s[2:end] end
+   n = length(s)
+   fullgroups, finalgroup = divrem(n, groupsize)
+   pretty = repeat(" ", n+(fullgroups-1)+(finalgroup!=0)+length(numsign))
+   
+   sourceidx = n
+   targetidx = length(pretty)
+   for k in fullgroups:-1:1
+       pretty[(targetidx-groupsize+1):targetidx] = s[(sourceidx-groupsize+1):sourceidx]
+       sourceidx -= groupsize
+       targetidx -= groupsize
+       if k > 1
+           pretty[targetidx] = separator
+           targetidx -= 1
+       end
+   end
+   
+   if finalgroup > 0
+       pretty[targetidx] = separator
+       targetidx -= 1
+       pretty[(targetidx-finalgroup+1):targetidx] = s[(sourceidx-finalgroup+1):sourceidx]
+       targetidx -= 1
+   end
+   if numsign == "-"
+       @assert targetidx == 1
+       pretty[targetidx] = "-"
+   end
+   
+   pretty
+end
+
+function fractionalString(s::String, groupsize::Int=5)
+   if "e" in s
+       sfrac, sexponent = split(s,"e")
+    else
+       sfrac, sexponent = s, ""
+    end
+    
+    pretty = reverse(integerString(reverse(sfrac), groupsize)))
+    
+    if sexponent != ""
+       join(pretty,sexponent,"e")
+    else
+       pretty
+    end
+end
+
+end
+
+end # module
